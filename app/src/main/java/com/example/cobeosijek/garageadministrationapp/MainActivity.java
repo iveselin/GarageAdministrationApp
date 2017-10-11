3b9e0fd8fa7fa2fa6c1a345b08a23ab8e3930326
iveselin <ivanveselin@hotmail.com>
2017-10-11 11:58:09 +0200

    added first activity

--- app/src/main/java/com/example/cobeosijek/garageadministrationapp/MainActivity.java
+++ app/src/main/java/com/example/cobeosijek/garageadministrationapp/MainActivity.java
@@ -1,13 +1,57 @@
 package com.example.cobeosijek.garageadministrationapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button inputCarButton, salaryCalculatorButton, refillExpendablesButton, checkBalanceButton;
+
+    ImageView garageLogoImageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setUI();
+    }
+
+    private void setUI() {
+
+        inputCarButton = (Button) findViewById(R.id.inputCarButton);
+        salaryCalculatorButton = (Button) findViewById(R.id.salaryCalculatorButton);
+        refillExpendablesButton = (Button) findViewById(R.id.refillExpendablesButton);
+        checkBalanceButton = (Button) findViewById(R.id.checkBalanceButton);
+        garageLogoImageView = (ImageView) findViewById(R.id.garageLogoImageView);
+
+        inputCarButton.setOnClickListener(this);
+        salaryCalculatorButton.setOnClickListener(this);
+        refillExpendablesButton.setOnClickListener(this);
+        checkBalanceButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.inputCarButton:
+
+                break;
+            case R.id.salaryCalculatorButton:
+
+                break;
+            case R.id.refillExpendablesButton:
+
+                break;
+            case R.id.checkBalanceButton:
+
+                break;
+        }
     }
 }
