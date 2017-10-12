219fd26cdf9323e64cfc0eaa6f0fbcc84efaa9d4
iveselin <ivanveselin@hotmail.com>
2017-10-12 11:51:53 +0200

    implemented inputCar activity

--- app/src/main/java/com/example/cobeosijek/garageadministrationapp/CarInputActivity.java
+++ app/src/main/java/com/example/cobeosijek/garageadministrationapp/CarInputActivity.java
@@ -1,15 +1,78 @@
 package com.example.cobeosijek.garageadministrationapp;
 
+import android.content.Intent;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
-public class CarInputActivity extends AppCompatActivity {
+public class CarInputActivity extends AppCompatActivity implements View.OnClickListener, RadioGroup.OnCheckedChangeListener {
+
+    EditText ownerNameET;
+    EditText ownerEmailET;
+
+    RadioGroup workNeededRG;
+    RadioButton workNeededRBTN;
+
+    Button submitBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_input);
+
+        setUI();
+        setListeners();
+    }
+
+    private void setUI() {
+
+        ownerEmailET = (EditText) findViewById(R.id.ownerEmailET);
+        ownerNameET = (EditText) findViewById(R.id.ownerNameET);
+        workNeededRG = (RadioGroup) findViewById(R.id.workNeededRG);
+        submitBTN = (Button) findViewById(R.id.submitCarBTN);
+        workNeededRG.check(R.id.mechanicRB);
+    }
+
+    private void setListeners() {
+        workNeededRG.setOnCheckedChangeListener(this);
+        submitBTN.setOnClickListener(this);
     }
 
 
+    @Override
+    public void onClick(View view) {
+        // TODO: 12/10/2017 first check if empty
+        if (TextUtils.isEmpty(ownerNameET.getText().toString())) {
+            ownerNameET.setError("You can't leave this empty");
+            return;
+        } else if (TextUtils.isEmpty(ownerEmailET.getText().toString())) {
+            ownerEmailET.setError("You cant leave this empty");
+            return;
+        }
+
+        String ownerName = ownerNameET.getText().toString();
+        String ownerEmail = ownerEmailET.getText().toString();
+        int workNeededFlag = workNeededRG.indexOfChild(workNeededRBTN);
+
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra(MainActivity.KEY_OWNER_NAME, ownerName);
+        resultIntent.putExtra(MainActivity.KEY_OWNER_EMAIL, ownerEmail);
+        resultIntent.putExtra(MainActivity.KEY_WORK_NEEDED, workNeededFlag);
+
+        this.setResult(RESULT_OK, resultIntent);
+
+        this.finish();
+
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
+        workNeededRBTN = (RadioButton) findViewById(i);
+    }
 }
--- app/src/main/java/com/example/cobeosijek/garageadministrationapp/MainActivity.java
+++ app/src/main/java/com/example/cobeosijek/garageadministrationapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.cobeosijek.garageadministrationapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -7,12 +8,20 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.example.cobeosijek.garageadministrationapp.working_on.Car;
+import com.example.cobeosijek.garageadministrationapp.working_on.WorkNeededEnum;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final int KEY_CAR_REQUEST = 2;
+    public static final String KEY_OWNER_NAME = "owner_name";
+    public static final String KEY_OWNER_EMAIL = "owner_email";
+    public static final String KEY_WORK_NEEDED = "work_needed";
 
     Button inputCarButton, salaryCalculatorButton, refillExpendablesButton, checkBalanceButton;
     ImageView garageLogoImageView;
 
-    Garage myGarage = new Garage();
+    Garage myGarage;
 
 
     @Override
@@ -23,15 +32,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         setUI();
         setListeners();
+        myGarage = new Garage();
     }
 
     private void setUI() {
 
-        inputCarButton = (Button) findViewById(R.id.inputCarButton);
-        salaryCalculatorButton = (Button) findViewById(R.id.salaryCalculatorButton);
-        refillExpendablesButton = (Button) findViewById(R.id.refillExpendablesButton);
-        checkBalanceButton = (Button) findViewById(R.id.checkBalanceButton);
-        garageLogoImageView = (ImageView) findViewById(R.id.garageLogoImageView);
+        inputCarButton = (Button) findViewById(R.id.inputCarBTN);
+        salaryCalculatorButton = (Button) findViewById(R.id.salaryCalculatorBTN);
+        refillExpendablesButton = (Button) findViewById(R.id.refillExpendablesBTN);
+        checkBalanceButton = (Button) findViewById(R.id.checkBalanceBTN);
+        garageLogoImageView = (ImageView) findViewById(R.id.garageLogoIV);
 
     }
 
@@ -46,21 +56,68 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-
         switch (view.getId()) {
-            case R.id.inputCarButton:
-                // TODO: 11/10/2017 go to car activity
+            case R.id.inputCarBTN:
+                Intent startCarInput = new Intent(getApplicationContext(), CarInputActivity.class);
+                this.startActivityForResult(startCarInput, KEY_CAR_REQUEST);
                 break;
-            case R.id.salaryCalculatorButton:
+            case R.id.salaryCalculatorBTN:
                 // TODO: 11/10/2017 calculate salary and show results in recycler view
                 break;
-            case R.id.refillExpendablesButton:
-                // TODO: 11/10/2017 show a list of expendables and on click add new items to it
+            case R.id.refillExpendablesBTN:
+                // TODO: 11/10/2017 show a list of expendables and on long click add new items to it
                 break;
-            case R.id.checkBalanceButton:
+            case R.id.checkBalanceBTN:
                 // TODO: 11/10/2017 think how to improve this
-                Toast.makeText(this, String.format("Your current bank balance is %.2f$", myGarage.getBankBalance()),Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, String.format("Your current bank balance is %.2f$", myGarage.getBankBalance()), Toast.LENGTH_SHORT).show();
                 break;
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == KEY_CAR_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                createCar(data.getExtras());
+            }
+        }
+    }
+
+    private void createCar(Bundle extras) {
+        String ownerName = null;
+        String ownerEmail = null;
+        WorkNeededEnum workNeeded = null;
+        // TODO: 12/10/2017 maybe put everything in one if so you are sure that you have everything...
+        if (extras.containsKey(KEY_OWNER_NAME)) {
+            ownerName = extras.getString(KEY_OWNER_NAME);
+        }
+
+        if (extras.containsKey(KEY_OWNER_EMAIL)) {
+            ownerEmail = extras.getString(KEY_OWNER_EMAIL);
+        }
+
+        if (extras.containsKey(KEY_WORK_NEEDED)) {
+
+            int workNeededFlag = extras.getInt(KEY_WORK_NEEDED);
+
+            switch (workNeededFlag) {
+                case 1:
+                    workNeeded = WorkNeededEnum.MECHANIC;
+                    break;
+                case 2:
+                    workNeeded = WorkNeededEnum.PAINTJOB;
+                    break;
+                case 3:
+                    workNeeded = WorkNeededEnum.BOTH;
+                    break;
+            }
+        }
+        Car carToFix = new Car(ownerName, ownerEmail, workNeeded);
+
+        String carTestOutput = String.format("%s, %s, %s", carToFix.getOwnerName(), carToFix.getOwnerEmail(), carToFix.getWorkNeeded());
+        Toast.makeText(this, carTestOutput, Toast.LENGTH_SHORT).show();
+
+        // TODO: 12/10/2017 fix the carToFix
     }
 }
