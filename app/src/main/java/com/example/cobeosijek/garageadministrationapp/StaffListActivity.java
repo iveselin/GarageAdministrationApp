26246f299f7d6342bd63cda135994320009aca35
iveselin <ivanveselin@hotmail.com>
2017-10-13 14:05:12 +0200

    fixed the calculate salary

--- app/src/main/java/com/example/cobeosijek/garageadministrationapp/StaffListActivity.java
+++ app/src/main/java/com/example/cobeosijek/garageadministrationapp/StaffListActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.cobeosijek.garageadministrationapp.staff.Person;
+import com.example.cobeosijek.garageadministrationapp.staff.Technician;
 
 import java.util.ArrayList;
 import java.util.Locale;
@@ -60,15 +61,21 @@ public class StaffListActivity extends AppCompatActivity implements PersonAdapte
     @Override
     public void onClick(View view, int position) {
 
+        int workCost;
         Person clickedPerson = getPeople().get(position);
+        if (clickedPerson.getClass() == Technician.class) {
+            workCost = 120;
+        } else {
+            workCost = 50;
+        }
+        String outputString = String.format(Locale.getDefault(), "%s worked for %d hour(s) and his salary is: %.2f",
+                clickedPerson.getEmployeeName(), clickedPerson.getWorkHours(), clickedPerson.getWorkHours() * workCost * 0.7);
 
-        String outputString = String.format(Locale.getDefault(), "%s worked for %d hours and his salary is: %.2f",
-                clickedPerson.getEmployeeName(), clickedPerson.getWorkHours(), clickedPerson.getWorkHours() * 50.00);
-        // TODO: 13/10/2017 reflect changes on main, and pull actual cost of work hour
+        // TODO: 13/10/2017 reflect changes on main
         getPeople().get(position).resetWorkHours();
         personAdapter.notifyDataSetChanged();
-        Toast.makeText(getApplicationContext(), outputString, Toast.LENGTH_LONG).show();
 
+        Toast.makeText(getApplicationContext(), outputString, Toast.LENGTH_LONG).show();
 
     }
 }
